7ddf6a06b4e02c7a5a357164033200c6c848be10
leemonlin <1131742104@qq.com>
2023-03-03 17:21:33 +0800

    add translate.py

--- ComputingEssential/translate.py
+++ ComputingEssential/translate.py
@@ -4,11 +4,16 @@
 # This code runs on Python 2.7.x and Python 3.x.
 # You may install `requests` to run this code: pip install requests
 # Please refer to `https://api.fanyi.baidu.com/doc/21` for complete api document
-
+import os 
 import requests
 import random
 import json
 from hashlib import md5
+import re
+
+'''
+    翻译类：提供api接口的访问，以及翻译结果的返回。
+'''
 class Translate():
     def __init__(self):
         
@@ -36,14 +41,56 @@ class Translate():
         payload = {'appid': self.appid, 'q': query, 'from': self.from_lang, 'to': self.to_lang, 'salt': salt, 'sign': sign}
 
         # Send request
-        r = requests.post(self.url, params=payload, headers=headers)
-        result = r.json()
+        try:
+            r = requests.post(self.url, params=payload, headers=headers)
+            result = r.json()['trans_result'][0]['dst']
+            # Show response
+            return result
+        except Exception as e:
+            print(e)
+        
+            
 
-        # Show response
-        print(json.dumps(result, indent=4, ensure_ascii=False))
+
+def trasns_process(trans,path):
+    basename = os.path.basename(path)
+    print("processing chapter: {}".format(os.path.basename))
+    with open(path,'r',encoding='utf-8') as f:
+        chapter = f.readlines()
+    for line in chapter:
+        line = line.replace("-------------------------------------------- 分页分隔 --------------------------------------------\n",'')
+        if line == ' ' or line =='\n'or line.find("reset page") != -1:
+            continue
+        result_line = trans.translate(line)
+        write_path = os.path.join(r'leemonlin/python/programs/KG-based-Auto_QA-System/ComputingEssential/book_text/translate',basename)
+        if not os.path.exists(write_path):
+            os.mknod(write_path)
+        else:
+            with open(write_path,'a',encoding='utf-8') as af:
+                        if result_line != None:
+                            print(result_line)
+                            af.write(result_line)
+    split(write_path)
+
+
+def split(path):
+    with open(path, "r", encoding="utf-8") as f:
+        text = f.read()
+    pattern = r'([。？！])'
+    result = re.split(pattern, text)
+    text_list =  ["".join(result[i:i+2]) for i in range(0, len(result), 2)]
+    with open(path, "w", encoding="utf-8") as f:
+        for sentence in text_list:
+            f.write(sentence.strip() + "\n")
+
+
 
 
 if __name__ == "__main__":
     # query = "this is a test" #这是我们需要放入章节
     trans = Translate()
-    trans.translate()
+    chapter_path = r'leemonlin/python/programs/KG-based-Auto_QA-System/ComputingEssential/book_text/chapter'
+    files = os.listdir(chapter_path)
+    for file in files:
+        trasns_process(trans,os.path.join(chapter_path,file))
+   
